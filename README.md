22a3ceaa4156abfbd85770011b6f7862b9b2a080
Narve Sætre <Narve Stre>
2016-02-25 11:37:08 +0100

    added installation sample file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # skag-server-templates
+
+
+Test locally on Windows
+=======================
+
+   C:\Users\narve\AppData\Local\Google\Chrome\Application\chrome.exe --disable-web-security --user-data-dir="C:/temp" file:///C:/dev/skag-server-templates/index.html
+   
+   
---
+++ installation-sample.js
@@ -0,0 +1,8 @@
+var installation = {
+  "id": "knowit-test",
+  "name": "Knowit Test Installation",
+  "image_url": null,
+  "yr_url": "http://www.yr.no/sted/Norge/Vest-Agder/Songdalen/Songdalen",
+  "text": "Dette er knowit sin test installasjon. beskrivelsen er hardkodet, ikke fra server. ",
+  "templatesInUse": ["intro", "dev", "yr"]
+};
