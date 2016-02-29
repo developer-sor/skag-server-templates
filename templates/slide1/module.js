6a22bc393dbde77fb112e22391170d34debcdf70
Walter Kjell Dahl <waldah@knowit.no>
2016-02-29 17:04:14 +0100

    La inn normalize for css og la til slide1

---
+++ templates/slide1/module.js
@@ -0,0 +1,23 @@
+﻿/**
+ * Created by narve on 2016-02-18.
+ */
+
+
+"use strict";
+
+console.log("Loading plugin ", "intro");
+
+var template = {
+    id: "slide1",
+    defaultTimeOut: 4000,
+    name: "Forside",
+    //canShow: function() { return true; },
+    prepareShow: function () { },
+    doShow: function (doneCB) {
+        console.log("Showing ", this.name, ", cb: ", doneCB);
+        setTimeout(doneCB, 5000)
+    },
+    endShow: function () { },
+};
+
+templateController.addTemplate(template);
---
+++ templates/slide1/slide1.js
@@ -0,0 +1,15 @@
+﻿
+
+function viewModel() {
+    var self = this;
+
+    this.clock = ko.observable(new Date().getHours() + ":" + new Date().getMinutes());
+
+    this.tick = function () {
+        self.clock(new Date().getHours() + ":" + new Date().getMinutes());
+    };
+
+    setInterval(self.tick, 3000);
+};
+
+ko.applyBindings(new viewModel());
