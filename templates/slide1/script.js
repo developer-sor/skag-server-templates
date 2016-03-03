0d06cd048d23ab87a8c6d8d7e82ab28117fe5160
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-03 13:36:37 +0100

    Ekstra script for slide1

---
+++ templates/slide1/script.js
@@ -0,0 +1,4 @@
+﻿$(function () {
+    $("#slideTitle").html(parent.installationData.name);
+    $("#textContainer").html(parent.installationData.description.replace(/(?:\r\n|\r|\n)/g, '<br />'));
+});
