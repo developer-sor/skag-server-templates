e9350927f8a237fddec4418e02347d0d165919b4
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-03 10:22:58 +0100

    La inn lagrin i localstorage for templatedata, men får ikke kontakt med server så får ikke sjekket med valid data fra server ennå. Venter til Narve kommer

---
+++ constants.js
@@ -0,0 +1,15 @@
+﻿
+
+function isNullOrEmpty(variabel) {
+    return variabel === null || variabel === undefined
+}
+
+
+
+var constants = {
+    api: 'http://skagerak-dpnmn.rhcloud.com/api/',
+    installation: 'installation/{id}',
+    client: 'installation/{id}/clients',
+    installationData: 'installationData',
+    installationTime: 'installationTime'
+}
--- index.js
+++ index.js
@@ -10,13 +10,49 @@ console.log("Loading index");
 var templateController = {
 // TODO: Fetch from server, cache in local storage
 //    templatesInUse: ["intro", "dev", "yr"],
-    templatesInUse: installation.templatesInUse,
+    templatesInUse: null,
 
     templates: [],
     templateIndex: 0,
     defaultTimeOut: 5000,
     addTemplate: function( templ ) {
         this.templates.push( templ );
+    },
+    setInstallationData: function(data){
+        window.localStorage.setItem(constants.installationTime, new Date());
+        window.localStorage.setItem(constants.installationData, JSON.stringify(data));
+    },
+    fetchDataFromServer: function(){
+        var url = constants.api + constants.installation.replace("{id}", installation.id);
+        $.ajax({
+            method: "GET",
+            url: url
+        })
+        .done(function (data) {
+            console.log('data ',data);
+            this.setInstallationData(data);
+        })
+        .fail(function (error) {
+            console.log('error ', error);
+            //fallback to local storage or retry x times?
+        });
+    },
+    hasRecentInstallationData: function(){
+        var date = window.localStorage.getItem(constants.installationTime);
+        var d1 = new Date();
+        var d2 = new Date(d1);
+        d2.setHours(d1.getHours() + 1);
+
+        return date !== null && date !== undefined && Date.parse(date) < d2.getTime();
+    },
+    hasValidInstallationData: function () {
+        var tempData = window.localStorage.getItem(constants.installationData);
+        if (isNullOrEmpty(tempData)) {
+            return false;
+        }
+        tempData = JSON.parse(tempData);
+        console.log(tempData);
+        return !isNullOrEmpty(tempData.image_url) && !isNullOrEmpty(tempData.templatesInUse);
     },
     findTemplate: function( templateIdString ) {
         var template = null;
@@ -48,15 +84,27 @@ var templateController = {
         self.showSlide(template);
     },
     start: function () {
-        var s = this.templates
+        if (this.hasRecentInstallationData() && this.hasValidInstallationData()) {
+            console.log("Found valid installationdata");
+            var data = JSON.parse(window.localStorage.getItem(constants.installationData));
+            this.templatesInUse = data.templatesInUse;
+        }
+        else {
+            console.log("Installationdata not retrieved yet. Attempting to fetch from server..");
+            this.fetchDataFromServer();
+        }
+
+        if (this.templatesInUse != null) {
+            var s = this.templates
             .map(function (t) {
                 return "<span id=" + t.id + ">" + t.name + "</span>";
             })
             .join("");
-        $("#template-list").html(s);
-        $("#master-header").html(installation.name);
+            $("#template-list").html(s);
+            $("#master-header").html(installation.name);
 
-        this.showSlide(this.findTemplate( this.templatesInUse[0]));
+            this.showSlide(this.findTemplate(this.templatesInUse[0]));
+        }
     },
 
     showSlide: function (template) {
