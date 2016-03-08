76419ea59a183656492a08262a5632a954fbdc95
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-08 14:25:59 +0100

    Fikset localstorage clear til å bli mer elegant

--- queryParameters.js
+++ queryParameters.js
@@ -12,11 +12,6 @@ if (getParameterByName('clear')) {
     var clearData = getParameterByName('clear');
     if (clearData && clearData == 'true') {
         console.log('clearing localstorage');
-        window.localStorage.removeItem(constants.installationTime);
-        window.localStorage.removeItem(constants.installationData);
-        window.localStorage.removeItem(constants.refChartTime);
-        window.localStorage.removeItem(constants.refChartData);
-        window.localStorage.removeItem(constants.yrTime);
-        window.localStorage.removeItem(constants.yrData);
+        window.localStorage.clear();
     }
 }
