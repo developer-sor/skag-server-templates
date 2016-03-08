e0e4aeebbe84245fded5401fa3c7b7cae253f6a3
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-08 14:03:41 +0100

    Oppdaterte installation sample

--- installation-sample.js
+++ installation-sample.js
@@ -1,8 +1,4 @@
 var installation = {
-  "id": "knowit-test",
-  "name": "Knowit Test Installation",
-  "image_url": null,
-  "yr_url": "http://www.yr.no/sted/Norge/Vest-Agder/Songdalen/Songdalen",
-  "text": "Dette er knowit sin test installasjon. beskrivelsen er hardkodet, ikke fra server. ",
-  "templatesInUse": ["intro", "dev", "yr"]
+    key: "Noden_Songdalen",
+    clientKey: "57"
 };
