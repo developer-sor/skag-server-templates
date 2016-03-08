dcdaf445e3e81c37c1e3758f59c8ee9a0035cadb
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-08 14:12:29 +0100

    La inn queryParamerters i egen fil og la inn egen parameter for clear hvis satt til true vil slette local storage. La js filen til i index.html

---
+++ queryParameters.js
@@ -0,0 +1,22 @@
+﻿if (getParameterByName('key')) {
+    installation.key = getParameterByName('key');
+    window.localStorage.removeItem(constants.installationTime);
+    window.localStorage.removeItem(constants.installationData);
+    window.localStorage.removeItem(constants.refChartTime);
+    window.localStorage.removeItem(constants.refChartData);
+    window.localStorage.removeItem(constants.yrTime);
+    window.localStorage.removeItem(constants.yrData);
+}
+
+if (getParameterByName('clear')) {
+    var clearData = getParameterByName('clear');
+    if (clearData && clearData == 'true') {
+        console.log('clearing localstorage');
+        window.localStorage.removeItem(constants.installationTime);
+        window.localStorage.removeItem(constants.installationData);
+        window.localStorage.removeItem(constants.refChartTime);
+        window.localStorage.removeItem(constants.refChartData);
+        window.localStorage.removeItem(constants.yrTime);
+        window.localStorage.removeItem(constants.yrData);
+    }
+}
