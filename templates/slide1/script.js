3218bf4882bb19ab1b7e867c2ce9dbf0521294c9
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-10 10:20:31 +0100

    La inn slik at overflow blir skjult + kode for at man kan kjøre slide1 i dev mode uten å dra opp alle slides

--- templates/slide1/script.js
+++ templates/slide1/script.js
@@ -1,4 +1,11 @@
-﻿$(function () {
+﻿
+//Only for dev
+//var installationData = "";
+//installationData = JSON.parse(window.localStorage.getItem(constants.installationData));
+//$('body').css({ 'background-image': "url(data:" + installationData.backgroundImage + ")" });
+
+
+$(function () {
     $("#slideTitle").html(parent.installationData.name);
     $("#textContainer").html(parent.installationData.description.replace(/(?:\r\n|\r|\n)/g, '<br />'));
 });
