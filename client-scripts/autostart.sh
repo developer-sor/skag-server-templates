cc3eaa9f667737c86feef980c898d33c098116b6
Narve Sætre <Narve Stre>
2016-03-15 14:31:42 +0100

    dokumentasjon

---
+++ client-scripts/autostart.sh
@@ -0,0 +1,12 @@
+
+## dette er /home/pi/.config/lxsession/LXDE-pi/autostart
+
+@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+# @xscreensaver -no-splash
+
+@xset s off
+@xset -dpms
+@xset s noblank
+
+/home/pi/fullscreen.sh
---
+++ client-scripts/fullscreen.sh
@@ -0,0 +1,7 @@
+killall epiphany-browser
+killall iceweasel
+# sleep 15 && sudo -u pi epiphany-browser -a --profile ~/.config  --display=:0 file:///home/pi/git/skag-server-templates/index.html >>/home/pi/ep-log.txt 2>&1 &
+sleep 15 && sudo -u pi iceweasel --display=:0 file:///home/pi/git/skag-server-templates/index.html >>/home/pi/ep-log.txt 2>&1 &
+sleep 20
+xte -x :0 'sleep 9' 'key F11'&
+xte 'mousemove 2000 200' -x:0
