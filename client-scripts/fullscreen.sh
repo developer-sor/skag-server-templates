45ebd3d65babe6f2659ef4597b8c2d3fc46772d0
Walter Kjell Dahl <walter@ultraclearance.com>
2016-03-29 16:36:02 +0200

    Oppdaterte fullscreen etter innspill fra Narve. Er en bug som sørger for at det kommer en popup når vi prøver å kjøre killall på iceweasel

--- client-scripts/fullscreen.sh
+++ client-scripts/fullscreen.sh
@@ -1,5 +1,5 @@
 killall epiphany-browser
-killall iceweasel
+killall -9 iceweasel
 # sleep 15 && sudo -u pi epiphany-browser -a --profile ~/.config  --display=:0 file:///home/pi/repo/index.html >>/home/pi/ep-log.txt 2>&1 &
 sleep 15 && sudo -u pi iceweasel --display=:0 file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
 sleep 20
