cbe20b020835503ceba7c43813087b2d044a9b03
Walter Kjell Dahl <walter@ultraclearance.com>
2016-04-04 14:28:40 +0200

    Test av fullscreen ved oppstart av iceweasle i stede for etterpå

--- client-scripts/fullscreen.sh
+++ client-scripts/fullscreen.sh
@@ -1,7 +1,7 @@
 killall epiphany-browser
 sudo -u pi killall -9 -w  iceweasel
 # sleep 15 && sudo -u pi epiphany-browser -a --profile ~/.config  --display=:0 file:///home/pi/repo/index.html >>/home/pi/ep-log.txt 2>&1 &
-sleep 15 && sudo -u pi iceweasel --display=:0 file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
+sleep 15 && sudo -u pi iceweasel --display=:0 file:///home/pi/repo/index.html?forceFetch=true -fullscreen >>/home/pi/ep-log.txt 2>&1 &
 sleep 20
 xte -x :0 'sleep 9' 'key F11'&
 xte 'mousemove 2000 200' -x:0
