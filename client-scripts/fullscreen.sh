d37b077c171546297f04a01fc178759ce1798855
TheLastCard <waldah@knowit.no>
2017-05-29 18:31:43 +0200

    La inn skjuling av restore bubble

--- client-scripts/fullscreen.sh
+++ client-scripts/fullscreen.sh
@@ -2,6 +2,6 @@ killall epiphany-browser
 sudo -u pi killall -9 -w  iceweasel
 # sleep 15 && sudo -u pi epiphany-browser -a --profile ~/.config  --display=:0 file:///home/pi/repo/index.html >>/home/pi/ep-log.txt 2>&1 &
 # sleep 15 && sudo -u pi iceweasel -fullscreen --display=:0 file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
-sleep 15 && sudo -u pi chromium-browser -kiosk --args --disable-web-security --test-type --user-data-dir=file:///home/pi/repo/ file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
+sleep 15 && sudo -u pi chromium-browser -kiosk --args --disable-web-security --disable-session-crashed-bubble --test-type --user-data-dir=file:///home/pi/repo/ file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
 sleep 20
 xte 'mousemove 2000 200' -x:0
