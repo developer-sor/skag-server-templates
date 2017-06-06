51072b4f9cfc6b686c26c695acac05edfe332e04
TheLastCard <waldah@knowit.no>
2017-06-06 09:16:09 +0200

    Fikset en bug i fullscreen-scriptet

--- client-scripts/fullscreen.sh
+++ client-scripts/fullscreen.sh
@@ -2,7 +2,6 @@ killall epiphany-browser
 sudo -u pi killall -9 -w  iceweasel
 # sleep 15 && sudo -u pi epiphany-browser -a --profile ~/.config  --display=:0 file:///home/pi/repo/index.html >>/home/pi/ep-log.txt 2>&1 &
 # sleep 15 && sudo -u pi iceweasel -fullscreen --display=:0 file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
-<<<<<<< HEAD
 
 #Sletting av Google Chromium tempfolder hvis denne finnes (tidligere feil)
 sudo rm -rf file:///home/pi/repo/client-scripts/file:
