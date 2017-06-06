5f472d4a11ab25655e16791852748da260c38561
TheLastCard <waldah@knowit.no>
2017-06-06 10:47:58 +0200

    Korrigering i scriptet igjen, denne gangen forhåpentligvis den siste

--- client-scripts/fullscreen.sh
+++ client-scripts/fullscreen.sh
@@ -7,6 +7,6 @@ sudo -u pi killall -9 -w  iceweasel
 sudo rm -rf file:///home/pi/repo/client-scripts/file:
 
 #--disable-infobars
-sleep 15 && sudo -u pi chromium-browser -kiosk --args --disable-web-security --disable-session-crashed-bubble --test-type --disable-translate --user-data-dir=file:///home/pi/ file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
+sleep 15 && sudo -u pi chromium-browser -kiosk --disable-web-security --restore-last-session --disable-infobars --disable-translate --user-data-dir=file:///home/pi/ file:///home/pi/repo/index.html?forceFetch=true >>/home/pi/ep-log.txt 2>&1 &
 sleep 20
 xte 'mousemove 2000 200' -x:0
